51340a51da3618c7cef09aa8cdcf181e625966c6
Jaewoong Lee <230@fumire.moe>
2018-05-08 09:55:54 +0900

    get the genes from FASTA file

---
+++ reader.py
@@ -0,0 +1,28 @@
+def cut_it(name):
+    if name[0] == ">":
+        name = name[1:]
+    return name.split()[0]
+
+def read(file_name="MERS_Korea_genome.fasta", full_name=False):
+    gene_list = dict()
+    pasta_file = open(file_name, "r").readlines()
+
+    name = ""
+    gene = ""
+    for line in pasta_file:
+        tmp = line.strip()
+        if tmp[0] == ">":
+            if name != "":
+                gene_list[name] = gene
+            if full_name:
+                name = tmp
+            else:
+                name = cut_it(tmp)
+            gene = ""
+        else:
+            gene += tmp
+
+    return gene_list
+
+if __name__ == "__main__":
+    print(read().keys())
