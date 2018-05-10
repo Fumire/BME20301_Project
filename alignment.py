4a6d4736d44c267923e9744df6b282a07943b06a
Jaewoong Lee <230@fumire.moe>
2018-05-10 23:42:47 +0900

    finish to make 'alignment.py'

--- alignment.py
+++ alignment.py
@@ -78,6 +78,8 @@ class Alignment:
     def solve(self):
         self.answer_global = calcMap_global(self.gene1, self.gene2)
         self.answer_local = calcMap_local(self.gene1, self.gene2)
+        self.find_alignment_global()
+        self.find_alignment_local()
 
     def print_all(self):
         self.print_global()
@@ -115,13 +117,13 @@ class Alignment:
                 print("%3s" % self.answer_local[i][j][1], end=' ')
             print()
 
-    def findAlignment_global(self):
+    def find_alignment_global(self):
         route =[[len(self.gene1)-1, len(self.gene2)-1, ""]]
         route_global = list()
 
         while len(route):
             x, y, tmp = route[0]
-            route = route[1:]
+            route[:] = route[1:]
             if x == 0 and y == 0:
                 route_global.append(tmp)
                 continue
@@ -150,9 +152,61 @@ class Alignment:
                     x, y = x-1, y
             self.subgene_global.append((subgene1, subgene2))
 
-    def showAlignment_global(self):
+    def find_alignment_local(self):
+        maximum_position = list()
+        for i in range(len(self.gene1)):
+            for j in range(len(self.gene2)):
+                if len(maximum_position) == 0:
+                    maximum_position.append([i, j, self.answer_local[i][j][0]])
+                elif maximum_position[0][2] == self.answer_local[i][j][0]:
+                    maximum_position.append([i, j, self.answer_local[i][j][0]])
+                elif maximum_position[0][2] < self.answer_local[i][j][0]:
+                    maximum_position[:] = []
+                    maximum_position.append([i, j, self.answer_local[i][j][0]])
+        route = list()
+        route_local = list()
+        for x, y, z in maximum_position:
+            route.append([x, y, ""])
+        while len(route):
+            x, y, tmp = route[0]
+            route[:] = route[1:]
+            if self.answer_local[x][y][1] == "*":
+                route_local.append(tmp)
+                continue
+            if self.answer_local[x][y][1].find("↖") != -1:
+                route.append([x-1, y-1, "↖"+tmp])
+            if self.answer_local[x][y][1].find("←") != -1:
+                route.append([x, y-1, "←"+tmp])
+            if self.answer_local[x][y][1].find("↑") != -1:
+                route.append([x-1, y, "↑"+tmp])
+        self.subgene_local = list()
+        for route in route_local:
+            x, y = len(self.gene1)-1, len(self.gene2)-1
+            subgene1, subgene2 = "", ""
+            for ch in route[::-1]:
+                if ch == "↖":
+                    subgene1 = self.gene1[x] + subgene1
+                    subgene2 = self.gene2[y] + subgene2
+                    x, y = x-1, y-1
+                elif ch == "←":
+                    subgene1 = "-" + subgene1
+                    subgene2 = self.gene2[y] + subgene2
+                    x, y = x, y-1
+                elif ch == "↑":
+                    subgene1 = self.gene1[x] + subgene1
+                    subgene2 = "-" + subgene2
+                    x, y = x-1, y
+            self.subgene_local.append((subgene1, subgene2))
+
+    def show_alignment_global(self):
         for i, gene in enumerate(self.subgene_global):
-            print(i)
+            print(i+2)
+            print(gene[0])
+            print(gene[1])
+
+    def show_alignment_local(self):
+        for i, gene in enumerate(self.subgene_local):
+            print(i+1)
             print(gene[0])
             print(gene[1])
 
@@ -160,5 +214,4 @@ if __name__ == "__main__":
     open_blosum()
     d1, d2 = "PAWHEAE", "HEAGAWGHEE"
     answer = Alignment(d1, d2)
-    answer.findAlignment_global()
-    answer.showAlignment_global()
+    answer.show_alignment_local()
